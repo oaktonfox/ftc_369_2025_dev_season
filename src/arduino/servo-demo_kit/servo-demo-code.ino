2bbdc6259bd8f855432abea80da38852339f9d38
Hao Liu <harryliuhao@hotmail.com>
2025-07-27 16:44:05 -0400

    new servo test kit script

---
+++ src/arduino/servo-demo_kit/servo-demo-code.ino
@@ -0,0 +1,64 @@
+#include <Adafruit_PWMServoDriver.h>
+#include <stdio.h>
+#include <stdlib.h> // Required for rand() and srand()
+#include <time.h>   // Required for time()
+Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);       // called this way, it uses the default address 0x40   
+
+#define SERVOMIN  125                                                 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  625                                                 // this is the 'maximum' pulse length count (out of 4096)
+
+const int number_of_servos = 8;
+int direction_array[number_of_servos];
+int servo_angle;
+int angle_increment = 10;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("16 channel Servo test!");
+  board1.begin();
+  board1.setPWMFreq(60);                  // Analog servos run at ~60 Hz updates
+
+}
+
+void loop() { 
+
+    // Generate a random number (0 or 1)
+  for(int i=0; i<number_of_servos;i++){
+    direction_array[i] = directionCode();
+    Serial.println(direction_array[i]);
+  }
+
+  
+// if direction is 1, starting is 0, and end is 180
+// if direction is -1, starting is 180, and end is 0
+  for(int i=0; i<number_of_servos; i++){
+    board1.setPWM(i, 0, angleToPulse(90+90*direction_array[i]));
+  }
+  delay(1000);
+    
+  for( int angle =0; angle<181; angle +=angle_increment){
+    for(int i=0; i<number_of_servos; i++){
+        servo_angle = 90+90*direction_array[i] - angle*direction_array[i]; 
+        board1.setPWM(i, 0, angleToPulse(servo_angle) );
+      }
+  }
+  delay(100);
+}
+
+int angleToPulse(int ang)                             //gets angle in degree and returns the pulse width
+  {  int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);  // map angle of 0 to 180 to Servo min and Servo max 
+     Serial.print("Angle: ");Serial.println(ang);
+     //Serial.print(" pulse: ");Serial.println(pulse);
+     return pulse;
+  }
+
+int directionCode() {
+    int random_binary = rand() % 2 ;
+
+    // Map to -1 or 1
+    int result = (random_binary == 0) ? -1 : 1;
+    //Serial.print("Random binary: ");Serial.println(random_binary);
+    //Serial.print("Random negative or positive 1: ");Serial.println(result);
+    return result;
+}
+
--- src/arduino/servo_sonic_integrated/servo_sonic_integrated.ino
+++ src/arduino/servo_sonic_integrated/servo_sonic_integrated.ino
@@ -12,10 +12,16 @@ Servo myservo1;  // create servo object to control a servo
 
 // servo parameters
 int pos = 0;    // variable to store the servo position
-int limit = 210;
-int step_delay = 20;
-int start_pos = 10;
-int end_pos = 65;
+int step_delay = 30;
+int current_pos;
+int analogPin = A3; // potentiometer wiper (middle terminal) connected to analog pin 3
+int val = 0;  // variable to store the value read
+
+// SWYFT: 10-65
+//Gobilda: 80-130
+//Axon 20-90
+int start_pos = 50;
+int end_pos = 90;
 const int servoPin = 11;
 
 //sonic sensor parameters
@@ -31,30 +37,48 @@ void setup() {
   myservo1.attach(servoPin);  // attaches the servo on pin 9 to the servo object
 }
 
+
+
 void loop() {
 
     for (pos = start_pos; pos <= end_pos; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo1.write(pos);            // tell servo to go to position in variable 'pos'
     delay(step_delay);                       // waits 15ms for the servo to reach the position
+    read_current_pos();
   }
   Serial.print("servo position: ");
   Serial.println(pos-1);
   distance = sonic_distance();
   
   for (pos = end_pos; pos >= start_pos; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo1.write(pos);               // tell servo to go to position in variable 'pos'
+    myservo1.write(pos);  
+         // tell servo to go to position in variable 'pos'
     delay(step_delay);                       // waits 15ms for the servo to reach the position
+    read_current_pos();
   }
-  Serial.print("servo position: ");
+
   Serial.println(pos+1);
   distance = sonic_distance();
 
 
+
 }
 
-int sonic_distance() {
 
+
+void read_current_pos() {
+
+  val = analogRead(analogPin); 
+  current_pos = val*360/3.3;
+
+  //Serial.print("servo current position: ");
+  //Serial.println(current_pos);
+
+
+}
+int sonic_distance() {
+  delay(200);
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -63,8 +87,8 @@ int sonic_distance() {
 
   duration = pulseIn(echoPin, HIGH);
   distance = (duration*.0343)/2;
-  Serial.print("Distance: ");
+  Serial.print("Sonic Distance: ");
   Serial.println(distance);
-  delay(100);
+  //delay(200);
   return distance;
 }
